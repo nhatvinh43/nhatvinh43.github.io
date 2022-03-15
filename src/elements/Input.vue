fdfd2a724f7f37036e2ab8a59bd0f66deb7ed2f0
vinhnphan <phannhatvinh10a@gmail.com>
2022-03-15 23:29:13 +0700

    Add input element

---
+++ src/elements/Input.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="label" v-if="label">{{ label }}</div>
+    <input class="input" :type="type" :min="min" :max="max" :value="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    min: {
+      type: Number,
+      default: null,
+    },
+    max: {
+      type: Number,
+      default: null,
+    },
+    value: {
+      type: Number | String,
+      default: null,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.label {
+  font-size: $type-size-caption;
+  font-weight: $type-font-bold;
+  color: $color-dark;
+  padding-left: $space-xs;
+}
+
+.input {
+  box-sizing: border-box;
+  width: 100%;
+  font-family: $type-font-body;
+  font-size: $type-size-body;
+  border-radius: $radius-sm;
+  padding: 10px 15px;
+  border: 1px solid $color-grey;
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
